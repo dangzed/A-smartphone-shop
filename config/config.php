24331e794a77dfa264a3aa8f8aa276c4bb8f594b
dangzed <dangvh99@gmail.com>
2021-06-09 15:22:30 +0700

    add config

--- config/config.php
+++ config/config.php
@@ -6,12 +6,11 @@ const DEVELOPMENT_ENVIRONMENT = true;
 define("PUBLIC_PATH", dirname(__FILE__) . "/../public");
 
 
-const DB_NAME = 'test';
-//    const DB_NAME = 'framework';
-const DB_USER = 'dangvh';
-const DB_PASSWORD = 'Vuhaidang157@';
-const DB_HOST = 'localhost';
+const DB_NAME = 'heroku_0668a692c23638b';
+const DB_USER = 'b422b2ac9415c4';
+const DB_PASSWORD = '67f4374d';
+const DB_HOST = 'us-cdbr-east-02.cleardb.com';
 
-const BASE_PATH = 'http://dangvh.com/webprog/Project';
+const BASE_PATH = 'http://shopfun.herokuapp.com/';
 
 const PAGINATE_LIMIT = '5';
