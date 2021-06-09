614bb4a9d0adacb333466046e7fff55db81bdfcc
dangzed <dangvh99@gmail.com>
2021-06-09 15:14:33 +0700

    push some more files

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/** Configuration Variables **/
+
+const DEVELOPMENT_ENVIRONMENT = true;
+define("PUBLIC_PATH", dirname(__FILE__) . "/../public");
+
+
+const DB_NAME = 'test';
+//    const DB_NAME = 'framework';
+const DB_USER = 'dangvh';
+const DB_PASSWORD = 'Vuhaidang157@';
+const DB_HOST = 'localhost';
+
+const BASE_PATH = 'http://dangvh.com/webprog/Project';
+
+const PAGINATE_LIMIT = '5';
